7fb2055c2ae6dc0ea4a7a69a37e90212ac35200b
ujjval.priyadarshi <ujjval.priyadarshi@lybrate.com>
2023-08-03 10:55:03 +0530

    Adding Seo Header

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,31 @@ import Header from '@/components/header'
 import { Metadata } from 'next'
 import { metaData } from '@/constant/title'
 import GifMaker from '@/components/GifMaker'
- 
+import SeoHeader from '@/components/SeoHeader'
+
 export const metadata: Metadata = {
   title: metaData.title,
-  description: metaData.description
+  description: metaData.description,
+  openGraph: {
+    title: metaData.title,
+    url: '/'
+  }
 }
 
 export default function Home() {
   return (
-    <main className="min-h-screen m-0 p-0 bg-black">
-      <Header />
-      <MainContainer>
-        <section className='pt-4 text-white'>
-          <h1 className='text-center text-2xl font-bold'>Gif Maker</h1>
-          <p className='text-center'>{metaData.description}</p>
-          <GifMaker />
-        </section>
-      </MainContainer>
-    </main>
+    <>
+      <SeoHeader />
+      <main className='min-h-screen m-0 p-0 bg-black'>
+        <Header />
+        <MainContainer>
+          <section className='pt-4 text-white'>
+            <h1 className='text-center text-2xl font-bold'>Gif Maker</h1>
+            <p className='text-center'>{metaData.description}</p>
+            <GifMaker />
+          </section>
+        </MainContainer>
+      </main>
+    </>
   )
 }
