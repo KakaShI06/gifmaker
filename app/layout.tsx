8d95a3ff78c82ffd526cc43d6a6043277a11434e
KakaShI06 <50545255+KakaShI06@users.noreply.github.com>
2024-01-25 12:19:55 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,18 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Simple Gif Generator from videos',
-  description: 'Make any video into the gif within seconds',
+  title: 'Convert Video into GIF within a second!',
+  description: 'Turn your favorite videos into shareable gifs with GifMaker. Our easy-to-use app lets you quickly convert videos to gifs without any hassle. Choose your favorite clip, select the start and end points, and let GifMaker do the rest. Share your newly created gifs with your friends and family on social media or messaging apps. With GifMaker, the possibilities are endless.',
+  alternates: {
+    canonical: 'https://gifmaker.snapbit.in'
+  },
+  openGraph: {
+    type: 'website',
+    siteName: 'GIF Maker',
+    title: 'Convert Video into GIF within a second!',
+    url: 'https://gifmaker.snapbit.in',
+    description: 'Turn your favorite videos into shareable gifs with GifMaker. Our easy-to-use app lets you quickly convert videos to gifs without any hassle. Choose your favorite clip, select the start and end points, and let GifMaker do the rest. Share your newly created gifs with your friends and family on social media or messaging apps. With GifMaker, the possibilities are endless.',
+  },
 }
 
 export default function RootLayout({
